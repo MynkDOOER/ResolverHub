224f554032a60a4b89710ee4d87f0852c08793f1
hocrux <mayank.sd2005@gmail.com>
2026-07-25 21:38:32 +0530

    Completed Project  API

---
+++ Backend/models/projectModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const projectSchema = new mongoose.Schema({
+    name : {
+        type:String,
+        required:true,
+    },
+    companyId:{
+        type:mongoose.Schema.Types.ObjectId,
+        default:null,
+        ref:'company'
+    },
+    description:{
+        type:String,
+        required:false
+    },
+
+}, {timestamps:true})
+
+const project = mongoose.model('project', projectSchema);
+export default project;
