ea3e3d84c6121ccc72fbe0a2995fc89207cf4f36
HarshitManocha <harshit.manocha13@gmail.com>
2026-07-25 21:49:11 +0530

    Frontend Page for Creating Company

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import { Route, Routes } from "react-router-dom"
-import Layout from "./layout/Layout"
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
-import Home from "./pages/Home"
-import Profile from './pages/profile'
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/profile";
+import CreateCompany from "./pages/createCompany";
 
 const App = () => {
-  return (
-    <>
-    <Routes>
-        <Route path='/' element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        </Route>
-    </Routes>
-    </>
-  )
-}
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/company/create" element={<CreateCompany />} />
+				</Route>
+			</Routes>
+		</>
+	);
+};
 
 export default App;
---
+++ Frontend/src/stores/authStore.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useAuthStore = create(
+	persist(
+		(set) => ({
+			token: null,
+			user: null,
+
+			login: (token, user) => set({ token, user }),
+
+			logout: () => set({ token: null, user: null }),
+
+			refreshUser: (user) => set({ user }),
+		}),
+		{
+			name: "auth-storage",
+		},
+	),
+);
+
+export default useAuthStore;
