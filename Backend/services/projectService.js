06c0fe1448b54059b03c3af6b86574bf80f21a9d
HarshitManocha <harshit.manocha13@gmail.com>
2026-07-28 23:00:31 +0530

    Solved Compile Time Errors

--- Backend/services/projectService.js
+++ Backend/services/projectService.js
@@ -1,184 +1,183 @@
 import mongoose from "mongoose";
 import {
-  createProject as createProjectRepo, // renamed to avoid recursive call
-  findProjectByCompanyId,
-  findProjectById,
-  findProjectByIdAndDelete,
-  findProjectByIdAndUpdate,
-  findProjectByName,
+	createProject as createProjectRepo, // renamed to avoid recursive call
+	findProjectByCompanyId,
+	findProjectById,
+	findProjectByIdAndDelete,
+	findProjectByIdAndUpdate,
+	findProjectByName,
 } from "../repositories/projectRepository.js";
 
 import {
-  findUserById,
-  findUserByIdAndUpdate,
-  updateManyUsers,
+	findUserById,
+	findUserByIdAndUpdate,
+	updateManyUsers,
 } from "../repositories/userRepository.js";
-import { useEffect } from "react";
 
 export const createProject = async (projectData, userId) => {
-  const user = await findUserById(userId);
+	const user = await findUserById(userId);
 
-  if (!user) {
-    throw new Error("User not found");
-  }
+	if (!user) {
+		throw new Error("User not found");
+	}
 
-  if (!user.companyId) {
-    throw new Error("User doesn't belong to a company");
-  }
+	if (!user.companyId) {
+		throw new Error("User doesn't belong to a company");
+	}
 
-  if (user.role !== "Admin") {
-    throw new Error("Only Admin can create Project");
-  }
+	if (user.role !== "Admin") {
+		throw new Error("Only Admin can create Project");
+	}
 
-  const existing = await findProjectByName(projectData.name, user.companyId);
+	const existing = await findProjectByName(projectData.name, user.companyId);
 
-  if (existing) {
-    throw new Error("Project already exists");
-  }
-  if (!projectData.projectAdminId) {
-    throw new Error("Please select a Project Admin");
-  }
+	if (existing) {
+		throw new Error("Project already exists");
+	}
+	if (!projectData.projectAdminId) {
+		throw new Error("Please select a Project Admin");
+	}
 
-  const projectAdmin = await findUserById(projectData.projectAdminId);
+	const projectAdmin = await findUserById(projectData.projectAdminId);
 
-  if (!projectAdmin) {
-    throw new Error("Project Admin not found");
-  }
+	if (!projectAdmin) {
+		throw new Error("Project Admin not found");
+	}
 
-  if (projectAdmin.companyId.toString() !== user.companyId.toString()) {
-    throw new Error("User does not belong to this company");
-  }
+	if (projectAdmin.companyId.toString() !== user.companyId.toString()) {
+		throw new Error("User does not belong to this company");
+	}
 
-  if (projectAdmin.projectId) {
-    throw new Error("User already belongs to another project");
-  }
+	if (projectAdmin.projectId) {
+		throw new Error("User already belongs to another project");
+	}
 
-  const session = await mongoose.startSession();
+	const session = await mongoose.startSession();
 
-  let projectCreated;
-  let updatedUser;
+	let projectCreated;
+	let updatedUser;
 
-  try {
-    await session.withTransaction(async () => {
-      [projectCreated] = await createProjectRepo(
-        {
-          ...projectData,
-          companyId: user.companyId,
-          createdBy: userId,
-        },
-        { session },
-      );
-      console.log(projectCreated);
+	try {
+		await session.withTransaction(async () => {
+			[projectCreated] = await createProjectRepo(
+				{
+					...projectData,
+					companyId: user.companyId,
+					createdBy: userId,
+				},
+				{ session },
+			);
+			console.log(projectCreated);
 
-      updatedUser = await findUserByIdAndUpdate(
-        projectData.projectAdminId,
-        {
-          projectId: projectCreated._id,
-          role: "ProjectAdmin",
-          adminId: projectData.projectAdminId,
-        },
-        { session },
-      );
-    });
-    return { projectCreated, updatedUser };
-  } catch (err) {
-    throw new Error(err.message, {
-      cause: err,
-    });
-  } finally {
-    session.endSession();
-  }
+			updatedUser = await findUserByIdAndUpdate(
+				projectData.projectAdminId,
+				{
+					projectId: projectCreated._id,
+					role: "ProjectAdmin",
+					adminId: projectData.projectAdminId,
+				},
+				{ session },
+			);
+		});
+		return { projectCreated, updatedUser };
+	} catch (err) {
+		throw new Error(err.message, {
+			cause: err,
+		});
+	} finally {
+		session.endSession();
+	}
 };
 
 export const updateProject = async (projectId, projectData, userId) => {
-  const project = await findProjectById(projectId);
+	const project = await findProjectById(projectId);
 
-  if (!project) {
-    throw new Error("Project not found");
-  }
+	if (!project) {
+		throw new Error("Project not found");
+	}
 
-  const user = await findUserById(userId);
+	const user = await findUserById(userId);
 
-  if (!user) {
-    throw new Error("User not found");
-  }
+	if (!user) {
+		throw new Error("User not found");
+	}
 
-  if (user.role !== "Admin") {
-    throw new Error("Only Admin can update projects");
-  }
+	if (user.role !== "Admin") {
+		throw new Error("Only Admin can update projects");
+	}
 
-  if (project.companyId.toString() !== user.companyId.toString()) {
-    throw new Error("Unauthorized");
-  }
+	if (project.companyId.toString() !== user.companyId.toString()) {
+		throw new Error("Unauthorized");
+	}
 
-  const allowedUpdates = {};
+	const allowedUpdates = {};
 
-  if (projectData.name) allowedUpdates.name = projectData.name;
+	if (projectData.name) allowedUpdates.name = projectData.name;
 
-  if (projectData.description)
-    allowedUpdates.description = projectData.description;
+	if (projectData.description)
+		allowedUpdates.description = projectData.description;
 
-  const updatedProject = await findProjectByIdAndUpdate(
-    projectId,
-    allowedUpdates,
-  );
+	const updatedProject = await findProjectByIdAndUpdate(
+		projectId,
+		allowedUpdates,
+	);
 
-  return updatedProject;
+	return updatedProject;
 };
 
 export const deleteProject = async (projectId, userId) => {
-  const project = await findProjectById(projectId);
+	const project = await findProjectById(projectId);
 
-  if (!project) {
-    throw new Error("Project not found");
-  }
+	if (!project) {
+		throw new Error("Project not found");
+	}
 
-  const user = await findUserById(userId);
+	const user = await findUserById(userId);
 
-  if (!user) {
-    throw new Error("User not found");
-  }
+	if (!user) {
+		throw new Error("User not found");
+	}
 
-  if (user.role !== "Admin") {
-    throw new Error("Only Admin can delete projects");
-  }
+	if (user.role !== "Admin") {
+		throw new Error("Only Admin can delete projects");
+	}
 
-  if (project.companyId.toString() !== user.companyId.toString()) {
-    throw new Error("Unauthorized");
-  }
-  await updateManyUsers({ projectId }, { projectId: null });
+	if (project.companyId.toString() !== user.companyId.toString()) {
+		throw new Error("Unauthorized");
+	}
+	await updateManyUsers({ projectId }, { projectId: null });
 
-  await findProjectByIdAndDelete(projectId);
+	await findProjectByIdAndDelete(projectId);
 };
 
 export const getProject = async (projectId, userId) => {
-  const project = await findProjectById(projectId);
-  if (!project) {
-    throw new Error("Project Not found");
-  }
-  const user = await findUserById(userId);
-  if (!user) {
-    throw new Error("User not found");
-  }
-  if (!user.companyId) {
-    throw new Error("User doesn't belong to a company");
-  }
-  if (project.companyId.toString() !== user.companyId.toString()) {
-    throw new Error("Unauthorized");
-  }
-  return project;
+	const project = await findProjectById(projectId);
+	if (!project) {
+		throw new Error("Project Not found");
+	}
+	const user = await findUserById(userId);
+	if (!user) {
+		throw new Error("User not found");
+	}
+	if (!user.companyId) {
+		throw new Error("User doesn't belong to a company");
+	}
+	if (project.companyId.toString() !== user.companyId.toString()) {
+		throw new Error("Unauthorized");
+	}
+	return project;
 };
 
 export const getAllProjects = async (userId) => {
-  const user = await findUserById(userId);
+	const user = await findUserById(userId);
 
-  if (!user) {
-    throw new Error("User not found");
-  }
+	if (!user) {
+		throw new Error("User not found");
+	}
 
-  if (!user.companyId) {
-    throw new Error("User doesn't belong to a company");
-  }
+	if (!user.companyId) {
+		throw new Error("User doesn't belong to a company");
+	}
 
-  return await findProjectByCompanyId(user.companyId);
+	return await findProjectByCompanyId(user.companyId);
 };
